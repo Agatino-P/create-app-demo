895e52ecf630f0c6e92144c9342e5c4f5581ed38
Agatino-P <57068089+Agatino-P@users.noreply.github.com>
2021-08-13 15:26:09 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  const users= ["a","b","c","d"];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit <code>src/App.js</code> and save! to reload.
+          </p>
+          <p>{users.map(u=><p>u<br/></p> )}</p>
+               <p>
+                 1<br/>
+                 2
+                 3
+               </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!!!!!!!
         </a>
       </header>
     </div>
